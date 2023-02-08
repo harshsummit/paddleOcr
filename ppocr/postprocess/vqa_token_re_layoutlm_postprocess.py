dedb3ff9e4d7a27a2f51c33d70d4ed672beeebb8
harshsummit <HDutt@thesummitgrp.com>
2023-02-08 14:44:15 +0530

    modified postprocess

--- ppocr/postprocess/vqa_token_re_layoutlm_postprocess.py
+++ ppocr/postprocess/vqa_token_re_layoutlm_postprocess.py
@@ -23,10 +23,10 @@ class VQAReTokenLayoutLMPostProcess(object):
 
     def __call__(self, preds, label=None, *args, **kwargs):
         print('preds',type(preds))
-        pred_relations = preds['pred_relations']
+        pred_relations = paddle.to_tensor(preds['pred_relations'])
         print(type(pred_relations))
-        # if isinstance(preds['pred_relations'], paddle.Tensor):
-        pred_relations = np.array(pred_relations)
+        if isinstance(preds['pred_relations'], paddle.Tensor):
+            pred_relations = pred_relations.numpy()
         pred_relations = self.decode_pred(pred_relations)
 
         if label is not None:
